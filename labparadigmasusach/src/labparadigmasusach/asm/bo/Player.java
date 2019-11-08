02ce837d288304cf0b42263b5fe2b3891d41bc63
did <coalme@gmail.com>
2019-11-07 21:39:55 -0300

    Se eliminan las clases Enemigo y Aliado, y se deja solo la clase Player, como abstraccion, es decir, el player puede ser Aliado o CPU

--- labparadigmasusach/src/labparadigmasusach/asm/bo/Player.java
+++ labparadigmasusach/src/labparadigmasusach/asm/bo/Player.java
@@ -1,6 +1,6 @@
 package labparadigmasusach.asm.bo;
 
-public class Enemigo {
+public class Player {
 	
 	private Integer id;
 	private Integer posicion;
@@ -8,7 +8,7 @@ public class Enemigo {
 	private Integer vida;
 	private String  nombreEquipo;
 	
-	public Enemigo(Integer id, Integer posicion, Integer distMaximaMov, Integer vida, String nombreEquipo) {
+	public Player(Integer id, Integer posicion, Integer distMaximaMov, Integer vida, String nombreEquipo) {
 		super();
 		this.id = id;
 		this.posicion = posicion;
@@ -16,40 +16,56 @@ public class Enemigo {
 		this.vida = vida;
 		this.nombreEquipo = nombreEquipo;
 	}
+
+	enum TipoPlayer{
+		ALIADO,
+		ENEMIGO
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getPosicion() {
 		return posicion;
 	}
+
 	public void setPosicion(Integer posicion) {
 		this.posicion = posicion;
 	}
+
 	public Integer getDistMaximaMov() {
 		return distMaximaMov;
 	}
+
 	public void setDistMaximaMov(Integer distMaximaMov) {
 		this.distMaximaMov = distMaximaMov;
 	}
+
 	public Integer getVida() {
 		return vida;
 	}
+
 	public void setVida(Integer vida) {
 		this.vida = vida;
 	}
+
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
+
 	public void setNombreEquipo(String nombreEquipo) {
 		this.nombreEquipo = nombreEquipo;
 	}
+
 	@Override
 	public String toString() {
-		return "Enemigo [id=" + id + ", posicion=" + posicion + ", distMaximaMov=" + distMaximaMov + ", vida=" + vida
+		return "Player [id=" + id + ", posicion=" + posicion + ", distMaximaMov=" + distMaximaMov + ", vida=" + vida
 				+ ", nombreEquipo=" + nombreEquipo + "]";
-	}
+	} 
 	
 }
--- labparadigmasusach/src/labparadigmasusach/asm/model/Member.java
+++ labparadigmasusach/src/labparadigmasusach/asm/model/Member.java
@@ -1,7 +1,25 @@
 package labparadigmasusach.asm.model;
 
+import java.util.List;
+
+import labparadigmasusach.asm.bo.Player;
+
 public class Member {
 	
+	private List<Player> aliado;
+	private List<Player> cpu;
 	
+	public List<Player> getAliado() {
+		return aliado;
+	}
+	public void setAliado(List<Player> aliado) {
+		this.aliado = aliado;
+	}
+	public List<Player> getCpu() {
+		return cpu;
+	}
+	public void setCpu(List<Player> cpu) {
+		this.cpu = cpu;
+	}
 	
 }
